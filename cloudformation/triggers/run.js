d5d833f978e8570aeca178846a596ad2e8f2efbc
David Kelley <david.kelley@johnlewis.co.uk>
2020-04-22 14:12:05 +0000

    fix: 🐛 passing through --name property to docker command

--- cloudformation/triggers/run.js
+++ cloudformation/triggers/run.js
@@ -8,7 +8,7 @@ const TEMPLATE_FILE_PATH = '/command/cloudformation/triggers/trigger.yml';
 const argv = minimist(process.argv.slice(2));
 
 const {
-  name: stackName,
+  name,
   token,
   ['event-pattern']: eventPattern,
   ['event-type']: eventType,
@@ -24,6 +24,13 @@ const parameters = {
 };
 
 const capabilities = ['CAPABILITY_IAM', 'CAPABILITY_AUTO_EXPAND'];
+
+const sanitizedRepositoryName = process.env.GITHUB_REPOSITORY.replace(
+  /[^a-z0-9]/g,
+  '-'
+);
+
+const stackName = `${sanitizedRepositoryName}--${name}`;
 
 const main = async () => {
   try {
