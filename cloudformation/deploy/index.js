1f230d2fe0b472912575d47b98d5c26c93d8fb35
davidkelley <david.james.kelley@gmail.com>
2020-04-30 16:27:27 +0100

    fix: 🐛 ensuring tags={} is an optional parameter on deploy

--- cloudformation/deploy/index.js
+++ cloudformation/deploy/index.js
@@ -58,6 +58,10 @@ const getChangeSetType = async (stackName) => {
 };
 
 const getStackTags = (json) => {
+  if (!json) {
+    return [];
+  }
+
   if (typeof json === 'string') {
     try {
       return getStackTags(JSON.parse(json));
@@ -138,14 +142,16 @@ const deployStack = async (
 
   const changeSetType = await getChangeSetType(stackName);
 
-  const body = await step.group('Retrieving packaged template', () => {
+  const body = await step.group('Retrieving packaged template', async () => {
     if (templateBody) {
       return templateBody;
     }
-    return getTemplateBody({
+    const resolvedBody = await getTemplateBody({
       artifactName,
       filepath: templateFilePath,
     });
+
+    return resolvedBody;
   });
 
   step.startGroup(`Creating ChangeSet on stack: ${stackName}`);
